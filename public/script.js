e6b07b45988d6a58bb24d033670b768c0de82dc0
Santiago Benenatti <sbenenatti@gmail.com>
2025-02-18 07:57:24 -0300

    cambios ver imagenes13

--- public/script.js
+++ public/script.js
@@ -131,7 +131,7 @@ function setupPagination(totalItems) {
 
 
 function verMasImagenes(objectId) {
-  window.location.href = `verImagenes.html?objectId=${objectId}`;
+  window.location.href = `verimagenes.html?objectId=${objectId}`;
 }
 
 function buildImageElement(imgUrl) {
